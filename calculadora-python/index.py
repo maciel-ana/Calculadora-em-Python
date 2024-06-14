8777f83ea13b211a8f251860fe25f3e7111ff232
Beatriz Baccaro <130668481+BiaBaccaro@users.noreply.github.com>
2024-06-14 11:24:55 -0300

    commit bia

--- calculadora-python/index.py
+++ calculadora-python/index.py
@@ -8,28 +8,44 @@ while True:
     print('2. Subtração')
     print('3. Multiplicação')
     print('4. Divisão')
-    print('5 sair')
-    
-    op = int(input('\n\t Opção: '))
+    print('5. Sair')
+
         
         # Entradas
         
+    op = int(input('\n\t Opção: '))   
+         
         # Processamento 
         
     if op == 1:
             print('\n\t\t\t -- Soma -- \n')
             num1 = float(input('Digite o primeiro número: '))
             num2 = float(input('Digite o segundo número: '))
-            resultado = num1 +  num2
+            resultado = num1 + num2
             print('Resultado: ', resultado)
             
     elif op == 2:
             print('\n\t\t\t -- Subtração -- \n')
             num1 = float(input('Digite o primeiro número: '))
-            num2 = floar(input('Digite o segundo númeoro: '))
+            num2 = float(input('Digite o segundo númeoro: '))
             resultado = num1 - num2 
             print('Resultado:  ', resultado)
-                
+
+    elif op == 3:
+            print('\n\t\t\t -- Multiplicação -- \n')
+            num1 = float(input('Digite o primeiro número: '))
+            num2 = float(input('Digite o segundo número: '))
+            resultado = num1 * num2
+            print('Resultado: ', resultado)
+
+    elif op == 4:
+            print('\n\t\t\t -- Divisão -- \n')
+            num1 = float(input('Digite o primeiro número: '))
+            num2 = float(input('Digite o segundo número: '))
+            resultado = num1 / num2
+            print('Resultado: ', resultado)
+            
+               
         # Saída
         
     elif op == 5:
